97df11b511d61ae78e8da83489688f2bd7e77e50
guilherme <domtoniolo@gmail.com>
2022-04-03 09:55:06 -0300

    first commit - old site

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+let link_instagram = document.getElementById("link-instagram");
+
+link_instagram.onmouseover = function(){
+    link_instagram.style.color="#5949b1";
+}
+
+link_instagram.onmouseleave = function(){
+    link_instagram.style.color="#c03cbf";
+}
